b5b792059dfbb448619fa039cd5f609486acaa20
Yumo <60574298+Bold-engi@users.noreply.github.com>
2022-10-17 22:07:39 +0100

    Update data_swapper.vhd

--- data_swapper.vhd
+++ data_swapper.vhd
@@ -1,10 +1,9 @@
 entity data_swapper is
   port(
+    -- ARM Cortex M0 signals ----
+    HRDATA : out std_logic_vector (31 downto o);
+    -- AHB Master records -------
     dmao : in ahb_dma_out_type;
-    HRDATA : out std_logic_vector (31 downto 0);
-  );
-end;
-  
-architecture sturcture of data_swapper is
-  
+    );
+ end;
   
