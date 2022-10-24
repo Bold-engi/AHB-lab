677b4263f50e0e1f6977f00777db064333e1887c
shuyewang <60443045+shuyewang@users.noreply.github.com>
2022-10-24 10:49:36 +0100

    Update statemachine.vhd

--- statemachine.vhd
+++ statemachine.vhd
@@ -1,6 +1,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+library grlib;
+use grlib.amba.all;
+use grlib.stdlib.all;
+use grlib.devices.all;
+library gaisler;
+use gaisler.misc.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity state_machine is
 	port(
